6091da5eb80192c5046bd67a6795a00d9beae650
Mário Hermes <mariochermes@outlook.com>
2022-09-30 13:16:11 -0300

    Adequação de nomes

--- Estudo/Cadastro.py
+++
@@ -1,9 +0,0 @@
-def cadastrar():
-    arq = open('base_aluno.txt', 'a')
-    nome = str(input('Nome: '))
-    matricula = int(input('Matrícula: '))
-    arq.write(f'Nome: {nome} | Matrícula: {matricula}')
-
-
-#Main
-cadastrar()
--- Estudo/Controle.py
+++
@@ -1,5 +0,0 @@
-def cabeçalho(msg):
-    tam = len(msg) + 8
-    print('—' * tam)
-    print(f'{msg:^{tam}}')
-    print('—' * tam)

b8d507051107a732d7db93bc2169ec2fcbe579ba
Mário Hermes <mariochermes@outlook.com>
2022-10-23 23:45:44 -0300

    Atualização dos exercícios

---
+++ exercicios/ex038-comparando-numeros/comparando-numeros.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+if n1 > n2:
+    print('O primeiro valor é maior do que o segundo valor')
+elif n2 > n1:
+    print('O segundo valor é maior do que o primeiro valor')
+else:
+    print('Os valores são íguais')

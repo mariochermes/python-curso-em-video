5de77959287aa1908fa7116f2991cf76ee751c83
Mário Hermes <mariochermes@outlook.com>
2022-10-23 23:58:16 -0300

    Atualização dos exercícios

--- exercicios/ex039-alistamento-militar/alistamento-militar.py
+++ exercicios/ex039-alistamento-militar/alistamento-militar.py
@@ -1,5 +1,12 @@
 from datetime import date
 
 ano = int(input('Ano de nascimento: '))
+idade = date.today().year - ano
 
-print(f'Quem nasceu em {ano} tem {} em {}')
+print(f'Quem nasceu em {ano} tem {idade} anos em {date.today().year}')
+if idade > 18:
+    print(f'Você já deveria ter se alistado a {idade - 18} anos')
+    print(f'Seu alistamento foi em {date.today().year - (idade - 18)}')
+else: 
+    print(f'Ainda faltam {18 - idade} anos para o alistamento')
+    print(f'Seu alistamento será em {date.today().year + (18 - idade)}')

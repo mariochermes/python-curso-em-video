5ffb3ed275b72ee8d75c589381813d329df51da0
Mário Hermes <mariochermes@outlook.com>
2022-11-02 20:09:34 -0300

    Atualização dos exercícios

--- exercicios/ex041-classificando-atletas/classificando-atletas.py
+++ exercicios/ex041-classificando-atletas/classificando-atletas.py
@@ -5,13 +5,13 @@ idade = date.today().year - nasc
 print(f'O atleta tem {idade} anos')
 
 print('Classificação: ', end='')
-if (idade <= 9):
+if idade <= 9:
     print('MIRIM')
-elif (idade <= 14):
+elif idade <= 14:
     print('INFANTIL')
-elif (idade <= 19):
+elif idade <= 19:
     print('JUNIOR')
-elif (idade <= 25):
+elif idade <= 25:
     print('SÊNIOR')
 else:
     print('MASTER')

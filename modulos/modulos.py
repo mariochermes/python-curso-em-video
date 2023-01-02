2cc5bb5583b772af67405198c4202b2dde0aab5d
Mário Hermes <mariochermes@outlook.com>
2023-01-02 10:30:05 -0300

    Atualização de exercícios

--- modulos/modulos.py
+++
@@ -1,9 +0,0 @@
-import Moeda
-
-preço = float(input('Digite o preço: '))
-print(f'A metade de {Moeda.moeda(preço)} é {Moeda.moeda(Moeda.metade(preço))}')
-print(f'O dobro de {Moeda.moeda(preço)} é {Moeda.moeda(Moeda.dobro(preço))}')
-print(f'Aumentando 10%, temos {Moeda.moeda(Moeda.aumentar(preço, 10))}')
-
-
-

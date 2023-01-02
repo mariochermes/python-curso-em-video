346ac58032380da85ce0cc962b2d013511ae5491
Mário Hermes <mariochermes@outlook.com>
2023-01-02 12:24:49 -0300

    Atualização de exercícios

--- exercicios/ex073-tuplas-com-futebol/tuplas-com-futebol.py
+++ exercicios/ex073-tuplas-com-futebol/tuplas-com-futebol.py
@@ -8,7 +8,7 @@ times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
 print('-=' * 15)
 print(f'Lista de times do Brasileirão: {times}')
 print('-=' * 15)
-print(f'Os 5 primeiros são {times[0:5]}')
+print(f'Os 5 primeiros são {times[:5]}')
 print('-=' * 15)
 print(f'Os 4 últimos são {times[-4:]}')
 print('-=' * 15)

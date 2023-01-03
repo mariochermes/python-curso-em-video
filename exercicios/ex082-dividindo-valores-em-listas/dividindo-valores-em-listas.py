3b5eed8fb05fe9a8371017000be04e5b6224647b
Mário Hermes <mariochermes@outlook.com>
2023-01-03 10:58:58 -0300

    Atualização de exercícios

---
+++ exercicios/ex082-dividindo-valores-em-listas/dividindo-valores-em-listas.py
@@ -0,0 +1,17 @@
+valores = []
+
+while True:
+    valores.append(int(input('Digite um valor: ')))
+
+    continuar = ' '
+    while continuar not in 'sn':
+        continuar = input('Quer continuar (S/N)? ').strip().lower()[0]
+    if continuar == 'n':
+        break
+
+pares = [v for v in valores if v % 2 == 0]
+impares = [v for v in valores if v % 2 != 0]
+
+print(f'A lista completa é {valores}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')

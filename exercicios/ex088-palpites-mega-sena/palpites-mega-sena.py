2bc553522b751a266ae8613681c2837931f2720e
Mário Hermes <mariochermes@outlook.com>
2023-01-04 14:00:23 -0300

    Atualização de exercícios

--- exercicios/ex088-palpites-mega-sena/palpites-mega-sena.py
+++ exercicios/ex088-palpites-mega-sena/palpites-mega-sena.py
@@ -1,11 +1,21 @@
 from time import sleep
+from random import randint
 
-print('—')
-print('MEGA SENA')
-print('—')
+jogo = []
+
+print('—' * 30)
+print(f"{'MEGA SENA':^30}")
+print('—' * 30)
 
 qtd_jogos = int(input('Quantos jogos você quer que eu sorteie? '))
 
-print('-=' * 3 + f'SORTEANDO {qtd_jogos} JOGOS' + '-=' * 3)
-for i in range(5):
+print('-=' * 3 + f' SORTEANDO {qtd_jogos} JOGOS ' + '-=' * 3)
+for i in range(qtd_jogos):
+    print(f'Jogo {i + 1}: ', end='')
+    for j in range(6):
+        v = randint(1, 100)
+        jogo.append(v)
+        print(v, end=' ')
+    print()
+print('-=' * 15)
     

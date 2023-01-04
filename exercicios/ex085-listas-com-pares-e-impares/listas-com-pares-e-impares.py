ab8dadefc021a1fd96b94d8d4c895595b89acc86
Mário Hermes <mariochermes@outlook.com>
2023-01-04 09:14:07 -0300

    Atualização de exercícios

---
+++ exercicios/ex085-listas-com-pares-e-impares/listas-com-pares-e-impares.py
@@ -0,0 +1,18 @@
+valores = [[], []]
+
+for i in range(7):
+    v = int(input(f'Digite o {i + 1}º valor: '))
+
+    if v % 2 == 0:
+        valores[0].append(v)
+    else:
+        valores[1].append(v)
+
+valores[0].sort()
+valores[1].sort()
+
+print('—' * 40)
+print('Os valores pares digitados foram: ', end='')
+print(*valores[0])
+print('Os valores ímpares digitados foram: ', end='')
+print(*valores[1])

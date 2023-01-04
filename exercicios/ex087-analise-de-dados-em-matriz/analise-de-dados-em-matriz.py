62b25bc36a25ea4543e158cf4edc365796369093
Mário Hermes <mariochermes@outlook.com>
2023-01-04 13:04:23 -0300

    Atualização de exercícios

--- exercicios/ex087-analise-de-dados-em-matriz/analise-de-dados-em-matriz.py
+++ exercicios/ex087-analise-de-dados-em-matriz/analise-de-dados-em-matriz.py
@@ -1,10 +1,12 @@
 matriz = [[], [], []]
+soma_pares = soma_terceira_col = 0
 
 for i in range(3):
     for j in range(3):
         while True:
-            matriz[i].append(int(input(f'Digite um valor entre 1 e 10 para [{i}, {j}]: ')))
-            if matriz[i][j] in range(1, 10):
+            n = int(input(f'Digite um valor entre 1 e 10 para [{i}, {j}]: '))
+            if n in range(1, 10):
+                matriz[i].append(n)
                 break
             else:
                 print('O número deve estar entre 1 e 10')
@@ -16,17 +18,13 @@ for l in matriz:
     print()
 print('—' * 50)
 
-soma_pares = soma_terceira_col = 0
+soma_pares = print(sum([sum([n for n in l if n % 2 == 0]) for l in matriz]))
+print(f'A soma dos valores pares é {soma_pares}')
 
 for l in range(3):
-    for c in range(3):
-            if matriz[l][c] % 2 == 0:
-                soma_pares += matriz[l][c]
-            if c == 2:
-                soma_terceira_col += matriz[l][c]
-
-print(f'A soma dos valores pares é {soma_pares}')
+    soma_terceira_col += matriz[l][2]
 print(f'A soma dos valores da terceira coluna é {soma_terceira_col}')
+
 print(f'O maior valor da segunda linha é {max(matriz[1])}')
 
 

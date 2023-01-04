e9bfba3d2badfece72fc853e2d64b35454f43d07
Mário Hermes <mariochermes@outlook.com>
2023-01-04 13:33:57 -0300

    Atualização de exercícios

---
+++ exercicios/ex088-palpites-mega-sena/palpites-mega-sena.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print('—')
+print('MEGA SENA')
+print('—')
+
+qtd_jogos = int(input('Quantos jogos você quer que eu sorteie? '))
+
+print('-=' * 3 + f'SORTEANDO {qtd_jogos} JOGOS' + '-=' * 3)
+for i in range(5):
+    

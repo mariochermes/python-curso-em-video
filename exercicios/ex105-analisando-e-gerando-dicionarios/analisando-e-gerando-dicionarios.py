994ee7aac55ca255a9a278440857f169a8183f60
Mário Hermes <mariochermes@outlook.com>
2023-01-06 19:07:29 -0300

    Atualização de exercícios

---
+++ exercicios/ex105-analisando-e-gerando-dicionarios/analisando-e-gerando-dicionarios.py
@@ -0,0 +1,17 @@
+def main():
+    resp = notas(5.5, 2.5, 1.5)
+    print(resp)
+
+
+def notas(*nota, sit=False):
+    dados = {}
+
+    dados['total'] = len(nota)
+    dados['maior'] = max(nota)
+    dados['menor'] = min(nota)
+    dados['média'] = sum(nota) / len(nota)
+
+    return dados
+
+
+main()

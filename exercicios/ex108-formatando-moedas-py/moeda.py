e4d17eb7c67a55ad2e5ef441c6b60d27dc054e9b
Mário Hermes <mariochermes@outlook.com>
2023-01-10 18:48:22 -0300

    Atualização de exercícios

--- exercicios/ex108-formatando-moedas-py/moeda.py
+++ exercicios/ex108-formatando-moedas-py/moeda.py
@@ -1,18 +1,31 @@
-def aumentar(preco, taxa):
-    return preco + (taxa / 100) * preco
+def aumentar(preco, taxa, form=False):
+    if form == True:
+        return moeda(preco + (taxa / 100) * preco)
+    else:
+        return preco + (taxa / 100) * preco
 
 
-def diminuir(preco, taxa):
-    return preco - (taxa / 100) * preco
+def diminuir(preco, taxa, form=False):
+    if form == True:
+        return moeda(preco - (taxa / 100) * preco)
+    else:
+        return preco - (taxa / 100) * preco
 
 
-def dobro(preco):
-    return preco * 2    
+def dobro(preco, form=False):
+    if form == True:
+        return moeda(preco * 2)
+    else:
+        return preco * 2 
 
 
-def metade(preco):
-    return preco / 2
+def metade(preco, form=False):
+    if form == True:
+        return moeda(preco / 2)
+    else:
+        return preco / 2
 
 
 def moeda(preco = 0, moeda='R$'):
-    return   f'{moeda}{preco}'.replace('.', ',')
+    return f'{moeda}{preco}'.replace('.', ',')
+    
--- exercicios/ex108-formatando-moedas-py/teste.py
+++ exercicios/ex108-formatando-moedas-py/teste.py
@@ -3,6 +3,6 @@ from moeda import *
 p = float(input('Digite o preço: '))
 taxa = 10
 
-print(f'A metade de {moeda(p)} é {moeda(metade(p))}')
-print(f'A dobro de {moeda(p)} é {moeda(dobro(p))}')
-print(f'Aumentando {taxa}%, temos {moeda(aumentar(p, taxa))}')
+print(f'A metade de {moeda(p)} é {metade(p, form=True)}')
+print(f'A dobro de {moeda(p)} é {dobro(p, form=True)}')
+print(f'Aumentando {taxa}%, temos {aumentar(p, taxa, form=True)}')
